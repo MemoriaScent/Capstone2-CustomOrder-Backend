67fca58e3dd3bb3ff97d33dd7e33fb5dd82175ad
yejin-e <yejin01e@gmail.com>
2024-05-13 15:30:32 +0900

    <Feat> 사용자 엔티티 생성

---
+++ src/entity/user.entity.ts
@@ -0,0 +1,38 @@
+import { Column } from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger';
+export class UserEntity {
+  @ApiProperty({
+    example: '1234@memoria.kr',
+    description: '로그인 메일',
+  })
+  @Column()
+  email: string;
+
+  @ApiProperty({
+    example: '12345678',
+    description: '비밀번호',
+  })
+  @Column()
+  password: string;
+
+  @ApiProperty({
+    example: '홍길동',
+    description: '이름',
+  })
+  @Column()
+  name: string;
+
+  @ApiProperty({
+    example: '01012341234',
+    description: '전화번호',
+  })
+  @Column()
+  phone: string;
+
+  @ApiProperty({
+    example: '부산광역시 부산진구 가야동 동의대학교 산학협력관 404호',
+    description: '주소',
+  })
+  @Column()
+  location: string;
+}

249abdf0eeae3753de2756d6b8e6bfd0d52236e5
yejin-e <yejin01e@gmail.com>
2024-05-17 13:27:09 +0900

    <Feat> 주문 엔티티 생성

---
+++ src/entity/order.entity.ts
@@ -0,0 +1,49 @@
+import { Column, Entity, OneToMany } from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger';
+import { DefaultEntity } from './default.entity';
+
+@Entity()
+export class UserEntity extends DefaultEntity {
+  @ApiProperty({
+    example: '1',
+    description: '주문한 사용자의 id값',
+  })
+  @Column()
+  @OneToMany(() => UserEntity, (u: UserEntity) => u.id)
+  userId: UserEntity;
+
+  @ApiProperty({
+    example: '2024-01-01',
+    description: '주문 날짜',
+  })
+  @Column()
+  orderDate: Date;
+
+  @ApiProperty({
+    example: '55000',
+    description: '할인 후 금액',
+  })
+  @Column()
+  price: number;
+
+  @ApiProperty({
+    example: '77000',
+    description: '배송비 포함 금액',
+  })
+  @Column()
+  orderPrice: number;
+
+  @ApiProperty({
+    example: '배송 완료',
+    description: '주문 진행 상황',
+  })
+  @Column()
+  status: string;
+
+  @ApiProperty({
+    example: '3',
+    description: '주문 수량',
+  })
+  @Column()
+  count: number;
+}

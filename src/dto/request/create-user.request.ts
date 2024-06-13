9d0c9765bb9c98feb1e6e670cc863df6e01f2043
StarJune <partis98@kakao.com>
2024-06-13 23:09:48 +0900

    <fix> User Creation DTO 수정

--- src/dto/request/create-user.request.ts
+++ src/dto/request/create-user.request.ts
@@ -1,46 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail, IsNumberString, IsString } from "class-validator";
+import { IsEmail, IsNumberString, IsString } from 'class-validator';
+import { UserEntity } from '../../entity/user.entity';
 
-export class CreateUserRequest {
-  @ApiProperty({
-    example: '1234@memoria.kr',
-    description: '이메일',
-  })
-  @IsEmail()
-  email: string;
-
-  @ApiProperty({
-    example: '12345678',
-    description: '비밀번호',
-  })
-  @IsString()
-  pw: string;
-
+export class CreateUserRequest extends UserEntity {
   @ApiProperty({
     example: '12345678',
     description: '비밀번호 재확인',
   })
   @IsString()
   pwCheck: string;
-
-  @ApiProperty({
-    example: '홍길동',
-    description: '이름',
-  })
-  @IsString()
-  name: string;
-
-  @ApiProperty({
-    example: '01012341234',
-    description: '전화번호',
-  })
-  @IsNumberString()
-  phone: string;
-
-  @ApiProperty({
-    example: '부산광역시 부산진구 가야동 동의대학교 산학협력관 404호',
-    description: '주소',
-  })
-  @IsString()
-  location: string;
 }
